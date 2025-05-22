c69bdbe1e6661d2588ce9336e2bc343fba946fb5
AniketXcodes <aniketcodes2003@gmail.com>
2025-05-23 03:04:32 +0530

    feature dark mode fixed

--- client/src/components/custom/FeatureHighlights.jsx
+++ client/src/components/custom/FeatureHighlights.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import { Sprout, HeartHandshake, Award, Truck } from "lucide-react";
+
 const FeatureHighlights = () => {
   return (
-    <div className="bg-white py-12 px-4 sm:px-10 flex flex-wrap justify-center items-center gap-20 text-center text-brown-800">
+    <div className="bg-muted py-12 px-4 sm:px-10 flex flex-wrap justify-center items-center gap-20 text-center text-foreground">
       <div className="w-40 flex flex-col items-center">
-        <Sprout size={40} className="text-brown-500 mb-2" />
+        <Sprout size={40} className="text-primary mb-2" />
         <p className="font-semibold text-sm">
           100% Pure and
           <br />
           Handpicked Saffron
         </p>
       </div>
-      <div className="hidden sm:flex w-40  flex-col items-center">
-        <HeartHandshake size={40} className="text-brown-500 mb-2" />
+      <div className="hidden sm:flex w-40 flex-col items-center">
+        <HeartHandshake size={40} className="text-primary mb-2" />
         <p className="font-semibold text-sm">
           Dedicated To The
           <br />
           Health of India!
         </p>
       </div>
-      <div className="hidden sm:flex w-40  flex-col items-center">
-        <Award size={40} className="text-brown-500 mb-2" />
+      <div className="hidden sm:flex w-40 flex-col items-center">
+        <Award size={40} className="text-primary mb-2" />
         <p className="font-semibold text-sm">
           Lab Tested
           <br />
@@ -28,7 +29,7 @@ const FeatureHighlights = () => {
         </p>
       </div>
       <div className="w-40 flex flex-col items-center">
-        <Truck size={40} className="text-brown-500 mb-2" />
+        <Truck size={40} className="text-primary mb-2" />
         <p className="font-semibold text-sm">
           Domestic
           <br />
